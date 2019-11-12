5beef7a6b562d293f7fab67ad83e2039fb962e41
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-11-11 18:46:09 -0700

    wrote tests for the dashnoard component and reached mvp

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -7,3 +7,16 @@ test("renders correctly", () => {
     expect(render(<Dashboard />)).toMatchSnapshot();
 })
 
+test('check if it shows the default controls and display', () => {
+        const { getByText } = render(<Dashboard />);
+        getByText(/open/i);
+        getByText(/unlocked/i);
+
+        getByText(/lock gate/i);
+        getByText(/close gate/i)
+});
+
+
+
+
+

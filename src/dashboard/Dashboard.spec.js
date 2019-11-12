279e335f9f8300636bfd5685f49b24487e07b1d5
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-11-11 17:22:09 -0700

    wrote tests for the display and controls component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
 // Test away
+import React from "react";
+import Dashboard from "./Dashboard";
+import { render } from "@testing-library/react"
+
+test("renders correctly", () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+})
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,44 @@
 // Test away!
+import React from "react"
+import Display from "./Display";
+import { render, cleanup } from "@testing-library/react"
+import '@testing-Library/jest-dom/extend-expect'
+
+
+
+afterEach(cleanup);
+
+test("renders correctly", () => {
+    expect(render(<Display />)).toMatchSnapshot();
+})
+test('opened and unlocked', () => {
+    const { getByText, queryByText } = render(<Display closed={false} locked={false} />);
+
+    const unlocked = getByText(/unlocked/i);
+    const open = getByText(/open/i);
+
+    expect(unlocked).toHaveClass('green-led')
+    expect(open).toHaveClass('green-led')
+
+});
+
+test('closed and unlocked', () => {
+    const { getByText } = render(<Display closed={true} locked={false} />);
+
+    const unlocked = getByText(/unlocked/i);
+    const closed = getByText(/closed/i);
+
+    expect(unlocked).toHaveClass('green-led')
+    expect(closed).toHaveClass('red-led');
+
+});
+
+test('closed and locked', () => {
+    const { getByText } = render(<Display closed={true} locked={true} />);
+
+    const locked = getByText(/locked/i);
+    const closed = getByText(/closed/i);
+
+    expect(locked).toHaveClass('red-led')
+    expect(closed).toHaveClass('red-led')
+})
